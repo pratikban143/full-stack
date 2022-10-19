2d1ee49bb4c4d7047220c453cdfcd8d43c616c3c
Pratik ban <105238379+pratikban143@users.noreply.github.com>
2022-10-20 00:13:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Detail from './Detail';
+
+function App() {
+  return (
+    <div className="App">
+      <Detail></Detail>
+    </div>
+  );
+}
+
+export default App;
---
+++ Detail.js
@@ -0,0 +1,64 @@
+
+const Detail = () => {
+
+    return (
+        <div>
+            <div className="container">
+
+                <div className="Details_Main" id="Item">Details
+                    <div className="Details_Main_Grid" id="A">
+                        <div className="HashTag grid_01" id="01"><span style={{ fontSize: "20px" }}>
+                            <h6 style={{ padding: "10px", marginTop: "10px" }}>Hashtag</h6>Enter Your Hashtag Here
+                        </span></div>
+                        <div className="HashTag grid_02" id="02"><span style={{ fontSize: "20px" }}>
+                            <h6 style={{ padding: "10px", marginTop: "10px" }}>Users</h6>235 users
+                        </span></div>
+                        <div className="HashTag grid_03" id="03"><span style={{ fontSize: "20px" }}>
+                            <h6 style={{ padding: "10px", marginTop: "10px" }}>Select Color</h6>Enter Your Hashtag Here
+                        </span></div>
+                        <div className="HashTag grid_04" id="04"><span style={{ fontSize: "20px" }}>
+                            <h6 style={{ padding: "10px", marginTop: "10px" }}>Bride's Name</h6>Enter Your Hashtag Here
+                        </span></div>
+                        <div className="HashTag grid_05" id="05"><span style={{ fontSize: "20px" }}>
+                            <h6 style={{ padding: "10px", marginTop: "10px" }}>Groom's Name</h6>Enter Your Hashtag Here
+                        </span></div>
+                        <div className="HashTag grid_06" id="06"><span style={{ fontSize: "20px" }}>
+                            <h6 style={{ padding: "10px", marginTop: "10px" }}>Wedding Date</h6>20-10-2022
+                        </span></div>
+                        <div className="HashTag grid_07" id="07"><span style={{ fontSize: "20px" }}>
+                            <h6 style={{ padding: "10px", marginTop: "10px" }}>Wedding Venue</h6><b>Enter your's Venue address here for the wedding</b>
+                        </span></div>
+                        <div className="HashTag grid_08" id="08"><span style={{ fontSize: "20px" }}>
+                            <h6 style={{ padding: "10px", marginTop: "10px" }}>Upload Invatation Card</h6>
+                        </span></div>
+
+                    </div>
+
+
+                </div>
+                <div className="notification" id="Item">Notification</div>
+                <div className="BannerImage" id="Item">Banner Image
+                    <h2>hello</h2>
+                </div>
+                <div className="GroomSide" id="Item">Groom Side
+                    <h2>hello</h2>
+                </div>
+                <div className="Gallery" id="Item">Gallery</div>
+                <div className="BrideSide" id="Item">Bride Side</div>
+                <div className="BrideSide2" id="Item">Bride Side</div>
+                <div className="GuestId" id="Item">Guest Id</div>
+                <div className="SocialMedia" id="Item">Social </div>
+                <div className="Regerstereddriver" id="Item">Registred </div>
+                <div className="Event" id="Item">Event</div>
+                <div className="Gallerys" id="Item">GALLERY
+                    <div className="Event1" id="Item">Event</div>
+                    <div className="Event2" id="Item">Event</div>
+                    <div className="Event3" id="Item">Event</div>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Detail
